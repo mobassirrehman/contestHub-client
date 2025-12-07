a7f0173adff1d91719b14b0ef737081a2fddc909
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-12-07 18:17:18 +0600

    firebase set-up, config env variable, authContext & authProvider added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./contexts/AuthProvider.jsx";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
   </StrictMode>
 );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center py-20">
+        Welcome to ContestHub
+      </h1>
+      <p className="text-center text-gray-500">
+        Home page content coming soon...
+      </p>
+    </div>
+  );
+};
+
+export default Home;
